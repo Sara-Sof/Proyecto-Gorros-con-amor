153893793116deb447c86d32e49f7e57dfcf3262
Sara-Sofia <160204016+Sara-Sof@users.noreply.github.com>
2024-11-25 03:52:23 -0500

    Mejora experiencia de usuario
    Cree la clase DesignMessageBox para personalizar las ventanas emergentes y que estas sean acordes con el diseño del programa y amigables.

---
+++ Sistema de Gestión de GQP/UI/DesignMessageBox .cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Sistema_de_Gestión_de_GQP
+{
+    public partial class DesignMessageBox : Form
+    {
+        private Label lblMensaje;
+        private Label lblTitulo;
+        private PictureBox pictureBox1;
+        private Button btn;
+
+        public DesignMessageBox(string mensaje, string titulo, Image imagen = null)
+        {
+
+            InitializeComponent();
+
+
+            lblMensaje.Text = mensaje;
+            lblTitulo.Text = titulo;
+
+            if (imagen != null)
+            {
+                pictureBox1.Image = imagen;
+            }
+            else
+            {
+                pictureBox1.Visible = false; // Ocultar si no hay imagen
+            }
+        }
+
+        public void InitializeComponent()
+        {
+            lblMensaje = new Label();
+            lblTitulo = new Label();
+            pictureBox1 = new PictureBox();
+            btn = new Button();
+
+        }
+    }
+}
